e5e0299f8f46213c1c392c15588c7bb04d20409f
steve <551619855@qq.com>
2016-12-23 19:14:46 +0800

    ar detect ok.

--- include/ArCode.hpp
+++ include/ArCode.hpp
@@ -13,5 +13,31 @@
 #include <opencv2/aruco.hpp>
 
 
+class ArCodePort{
+public:
+    ArCodePort(int aruco_type)
+    {
+        dictionary_ptr_ =cv::Ptr<cv::aruco::Dictionary>(cv::aruco::getPredefinedDictionary(aruco_type)) ;
+    }
+
+    int DetectMarkers(cv::Mat src_image,std::vector<std::vector<Point2f>> corners,std::vector<int> ids,cv::Mat *res)
+    {
+        cv::aruco::detectMarkers(src_image,dictionary_ptr_,corners,ids);
+        if(ids.size() > 0)
+        {
+            cv::aruco::drawDetectedMarkers(*res,corners,ids);
+
+        }
+        return ids.size();
+    }
+
+
+
+private:
+    cv::Ptr<cv::aruco::Dictionary> dictionary_ptr_;
+
+    std::vector<std::vector<cv::Point2f>> corners_;
+    std::vector<int> ids_;
+};
 
 #endif //CVREADER_ARCODE_HPP
--- include/SerialSimple.hpp
+++ include/SerialSimple.hpp
@@ -7,6 +7,34 @@
 #ifndef CVREADER_SERIALSIMPLE_HPP
 #define CVREADER_SERIALSIMPLE_HPP
 
+#include <iostream>
+#include "serialsom.h"
+
+class SerialControl{
+public:
+
+    SerialControl(std::string port_name)
+    {
+        port_name_ = port_name;
+    }
+
+
+    int SetAngle(uint8_t picth_angle, uint8_t yaw_angle)
+    {
+        int fd;
+        bool true_false;
+        Serialport Serialport1(port_name_);
+        fd = Serialport1.open_port(port_name_);
+        Serialport1.set_opt(9600, 8, 'N', 1);
+        for(int i = 0; i<3; i++)
+            true_false = Serialport1.usart3_send(picth_angle, yaw_angle);
+        close(fd);
+        return true_false;
+    }
+
+private:
+    std::string port_name_;
+};
 
 
 #endif //CVREADER_SERIALSIMPLE_HPP
