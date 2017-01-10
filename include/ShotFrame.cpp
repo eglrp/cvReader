a61956041ccafed9cc959a98f402ef73a72a35c3
steve <5161985@qq.com>
2017-01-10 13:26:20 +0800

    ...

--- include/ShotFrame.cpp
+++ include/ShotFrame.cpp
@@ -12,5 +12,14 @@ ShotFrame::ShotFrame(std::string video_id, std::string control_id) {
     pitch_ = 0;
     yaw_ = 0;
 
-    serial_handle_ =
+
+    //TODO: Neet a completed method to finish this open port process.
+    serial_handle_ = open(control_id.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+    if (isatty(serial_handle_) == 0) {
+        std::cout << "Standard inpute is not a termianl device!" << std::endl;
+
+    }
+
+
+
 }
